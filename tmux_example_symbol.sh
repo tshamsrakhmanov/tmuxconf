715fcdee1d7414faaada8b75fb57e3de4a94ff0e
timur <timur@timur.ru>
2024-10-11 21:02:15 +0400

    commit

--- tmux_example_symbol.sh
+++ tmux_example_symbol.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
-printf '%b\n' '\uE0B1'
+
+input_flag=$1
+
+if [[ $input_flag == 'lh' ]]; then
+	printf '%b\n' '\uE0B6'
+elif [[ $input_flag == 'rh' ]]; then
+	printf '%b\n' '\uE0B0'
+fi
