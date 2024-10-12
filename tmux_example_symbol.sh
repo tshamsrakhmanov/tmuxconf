4f48080585cabdca354e275414138945d5eec3c3
timur <timur@timur.ru>
2024-10-12 10:42:31 +0400

    решение проблемы не запуска sh скриптов

--- tmux_example_symbol.sh
+++ tmux_example_symbol.sh
@@ -1,9 +1,9 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 input_flag=$1
 
 if [[ $input_flag == 'lh' ]]; then
-	printf '%b\n' '\uE0B6'
+	printf '%b\n' '\uE0B2'
 elif [[ $input_flag == 'rh' ]]; then
 	printf '%b\n' '\uE0B0'
 fi
