320640fd54fa3d02f58a14b51e37b55554e70347
Tim <tim@tim.com>
2024-10-15 13:24:43 +0400

    window-status moved to center, formatted in pills-style. right-status formatted in more compact way

--- tmux_example_symbol.sh
+++ tmux_example_symbol.sh
@@ -6,4 +6,8 @@ if [[ $input_flag == 'lh' ]]; then
 	printf '%b\n' '\uE0B2'
 elif [[ $input_flag == 'rh' ]]; then
 	printf '%b\n' '\uE0B0'
+elif [[ $input_flag == 'round_lh' ]]; then
+	printf '%b\n' '\uE0B6'
+elif [[ $input_flag == 'round_rh' ]]; then
+	printf '%b\n' '\uE0B4'
 fi
