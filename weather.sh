ca341cdd402ec91da23307bfb60da77e684de041
Tim <tim@tim.com>
2024-10-23 13:46:35 +0400

    weather complite

--- weather.sh
+++ weather.sh
@@ -1,14 +1,15 @@
 #! /usr/bin/env bash
 set -euo pipefail
 
-input_flag=$1
+
+function record_data {
 
 response=$(curl -s https://api.open-meteo.com/v1/forecast\?latitude\=56.863\&longitude\=53.1851\&current\=temperature_2m,apparent_temperature,weather_code,is_day\&timezone\=Europe%2FSamara)
 
-#weather_code=$(echo "$response" | jq -r ".current.weather_code")
-weather_code=$(echo "$input_flag")
+weather_code=$(echo "$response" | jq -r ".current.weather_code")
 temp=$(echo "$response" | jq -r ".current.temperature_2m" | cut -d '.' -f 1)
-appr_temp=$(echo "$response" | jq -r ".current.apparent_temperature" | cut -d '.' -f 1)
+# option to include to summary string - temp as feels
+#appr_temp=$(echo "$response" | jq -r ".current.apparent_temperature" | cut -d '.' -f 1)
 is_day=$(echo "$response" | jq -r ".current.is_day")
 
 THERMO="\U1F321"
@@ -22,25 +23,16 @@ thunder_codes=(95 96 99)
 
 if   [[ ${overcast_codes[@]}      =~ $weather_code ]];then
 weather_sign="Облачно \U26C5"
-
-elif [[ ${rain_codes[@]}          =~ $weather_code ]]; then
+elif [[ ${rain_codes[@]}          =~ $weather_code ]];then
 weather_sign="Дождь \U1F327"
-
-#elif [[ ${overcast_codes[@]}      =~ $weather_code ]];then
-#weather_sign="Облачно \U26C5"
-
 elif [[ ${fog_codes[@]}           =~ $weather_code ]];then
 weather_sign="Туман \U1F32B"
-
 elif [[ ${freezing_rain_codes[@]} =~ $weather_code ]];then
 weather_sign="Ледяной дождь \U2744 \U1F327"
-
 elif [[ ${snow_codes[@]}          =~ $weather_code ]];then
 weather_sign="Снег \U2744"
-
 elif [[ ${thunder_codes[@]}       =~ $weather_code ]];then
 weather_sign="Гром \U1F329"
-
 elif [[ $weather_code -eq 0 ]];then
 weather_sign="Ясно \U2600"
 
@@ -48,6 +40,35 @@ else
 weather_sign="\U1F7E5"
 fi
 
-summary_string="$weather_sign $THERMO $temp°C (Ощущ. $appr_temp°C)"
+summary_string="$weather_sign $THERMO $temp°C"
 
-printf "%b\n" "$summary_string"
+echo "CURRENT_DATE $(date +"%d-%m-%y-%H")" >> ~/tmuxconf/.weather
+printf "%b\n" "$summary_string" >> ~/tmuxconf/.weather
+
+}
+
+
+function main {
+
+if [[ -e ~/tmuxconf/.weather ]];then
+	reader
+else
+	touch ~/tmuxconf/.weather
+	record_data
+	reader
+fi
+}
+
+
+function reader {
+
+if [[ "$(date +"%d-%m-%y-%H")" = "$(cat ~/tmuxconf/.weather | grep "CURRENT_DATE" | awk '{print$2}')" ]]; then
+	echo $(tail -1 ~/tmuxconf/.weather)
+else
+	rm ~/tmuxconf/.weather
+	record_data
+fi
+
+}
+
+main
